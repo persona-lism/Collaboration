c7261ed645b1d2389a2f0a4e9bcab66b9daf5c8f
BabangHJ <hansenj5206@gmail.com>
2023-12-13 22:47:41 +0700

    ...

--- main/styles/page-function/first-page.js
+++ main/styles/page-function/first-page.js
@@ -1,40 +1,40 @@
-let slideIndex = 0;
+// let slideIndex = 0;
 
-const Imglist = document.querySelectorAll(".imgSlider");
-const Prev = document.getElementById("left-slider");
-const Next = document.getElementById("right-slider");
+// const Imglist = document.querySelectorAll(".imgSlider");
+// const Prev = document.getElementById("left-slider");
+// const Next = document.getElementById("right-slider");
 
 
-showSlide(slideIndex)
+// showSlide(slideIndex)
 
-Prev.addEventListener("click",function(){
-    plusDivs(-1);
-});
+// Prev.addEventListener("click",function(){
+//     plusDivs(-1);
+// });
 
-Next.addEventListener("click",function(){
-    plusDivs(1);
-})
+// Next.addEventListener("click",function(){
+//     plusDivs(1);
+// })
 
-function plusDivs(n) {
-    slideIndex += n;
-    showSlide(slideIndex);
-}
+// function plusDivs(n) {
+//     slideIndex += n;
+//     showSlide(slideIndex);
+// }
 
-function showSlide(n) {
-    if (n >= Imglist.length) {
-        slideIndex = 0;
-    } else if (n < 0) {
-        slideIndex = Imglist.length - 1
-    }
+// function showSlide(n) {
+//     if (n >= Imglist.length) {
+//         slideIndex = 0;
+//     } else if (n < 0) {
+//         slideIndex = Imglist.length - 1
+//     }
 
-    // for (let i = 0; i < Imglist.length; i++) {
-    //     Imglist[i].style.display = "none";
-    // }
+//     for (let i = 0; i < Imglist.length; i++) {
+//         Imglist[i].style.display = "none";
+//     }
 
     
 
-    Imglist[slideIndex].style.display = "flex"
-}
+//     Imglist[slideIndex].style.display = "flex"
+// }
 
 
 
