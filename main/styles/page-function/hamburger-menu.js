603682fcfa3a29636996bdab7532ff190cf4d01f
Personalism <edwinhermili@gmail.com>
2023-12-20 18:29:35 +0700

    Updated

--- main/styles/page-function/hamburger-menu.js
+++ main/styles/page-function/hamburger-menu.js
@@ -16,3 +16,20 @@ function hamburgerMenuAnimation() {
       animation.style.transform = "matrix(0, 1, 1, 0, 0, 0)";
     }
 }
+
+function responsiveMenu(x) {
+  var animation = document.getElementById("hamburger-menu");
+  var menu = document.getElementById("hamburger-content");
+  if (responsiveWidth768px.matches) {
+    menu.style.display = "none";
+    animation.style.transform = "none";
+  } else {
+    menu.style.display = "none";
+    animation.style.transform = "none";
+  }
+}
+
+var responsiveWidth768px = window.matchMedia("(min-width: 768px)");
+
+responsiveMenu(responsiveWidth768px);
+responsiveWidth768px.addListener(responsiveMenu)
